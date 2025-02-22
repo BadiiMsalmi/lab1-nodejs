08440caab10105a7b02525e95ae7313ebd70f70e
BadiiMsalmi <badiimsalmi2@gmail.com>
2025-02-22 14:59:55 +0100

    third commit

---
+++ first-app-mongoose-express/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+require('dotenv').config()
+
+const PORT = process.env.PORT || 5000
+const MONGO_URI = process.env.MONGO_URI
+
+
+mongoose.connect(MONGO_URI).then(()=>{
+    console.log('connected to mongodb')
+}).catch(err=>{
+    console.log(err)
+})
+
+app.listen(PORT,()=>{
+    console.log('listening on port',PORT)
+})
